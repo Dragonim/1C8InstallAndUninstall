e6571fcc92ded3039652405d15443965aabc9890
Dragonim <Dragonim@mail.ru>
2014-10-28 11:16:09 +0500

    добавлен 1C8InstallAndUninstall.cmd

---
+++ 1C8InstallAndUninstall.cmd
@@ -0,0 +1,6 @@
+﻿@rem для запуска powershell скриптов необходимо переопределить политику безопасности запуска powershell
+	@start /wait powershell "Set-ExecutionPolicy RemoteSigned -Force"
+@rem запускаем скрипт с нужными параметрами
+	@powershell "\\Server\1CDistr\1C8InstallAndUninstall.ps1" -dd "\\Server\1CDistr" -dl "\\Server\1CLog" -dp ael -ip last
+@rem возвращяем политику безопасности в значение по умолчанию
+	@start /wait powershell "Set-ExecutionPolicy Restricted -Force"
