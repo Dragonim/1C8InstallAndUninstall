50e5463519d8b31ab6177dab8943dda8631f481a
Dim <Dragonim@users.noreply.github.com>
2020-05-07 00:12:19 +0500

    Ослаблено ограничение перед запуском PS скрипта

--- 1C8InstallAndUninstall.bat
+++ 1C8InstallAndUninstall.bat
@@ -1,5 +1,5 @@
 @rem для запуска powershell скриптов необходимо переопределить политику безопасности запуска powershell
-	@start /wait powershell "Set-ExecutionPolicy RemoteSigned -Force"
+	@start /wait powershell "Set-ExecutionPolicy Bypass -Force"
 @rem запускаем скрипт с нужными параметрами
 	@powershell "\\Server\1CDistr\1C8InstallAndUninstall.ps1" -dd '\\Server\1CDistr' -dl '\\Server\1CLogs' -ip 'last' -dp 'ael' -iod 'DESIGNERALLCLIENTS=1 THINCLIENT=1 THINCLIENTFILE=1'
 @rem возвращяем политику безопасности в значение по умолчанию
